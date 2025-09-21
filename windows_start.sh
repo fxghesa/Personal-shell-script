25abd152bec11f83b46fc4a950a8e025f44e0ab2
fxghesa <gcoltonwhite84@gmail.com>
2025-09-21 18:29:23 +0700

    add start windows as vm
    Signed-off-by: fxghesa <gcoltonwhite84@gmail.com>

---
+++ windows_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd Program/windows_container/
+export PIPEWIRE_RUNTIME_DIR=/run/user/1000
+qemu-system-x86_64 -M q35,usb=on,acpi=on,hpet=off -m 32G -cpu host,hv_relaxed,hv_frequencies,hv_vpindex,hv_ipi,hv_tlbflush,hv_spinlocks=0x1fff,hv_synic,hv_runtime,hv_time,hv_stimer,hv_vapic -smp cores=12 -accel kvm -drive file=win10.qcow2 -device usb-tablet -device VGA,vgamem_mb=256 -nic user,model=e1000 -monitor stdio \
+    -audiodev pipewire,id=audiodev1 \
+    -device AC97,audiodev=audiodev1
+
+
